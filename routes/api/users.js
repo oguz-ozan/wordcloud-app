3fb2b272bff0decc732707ccc50048c093e0ca3a
oguz-ozan <oguz.ozan@ceng.deu.edu.tr>
2020-10-08 14:32:42 +0300

    ad structure

---
+++ routes/api/users.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
+
+// @route POST api/users
+// @desc Register user
+// @access Public
+router.post('/',[
+    check('email','Email is required').isEmail(),
+    check('password', 'Enter a valid password').isLength({min: 6})
+], async (req,
+                res) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    const { email, password } = req.body;
+
+    try{
+    // See if user exists
+        let user = await User.findOne({ email: email});
+
+        if(user){
+            return res.status(400).json({errors: [{msg: 'User already exist'}] });
+        }
+
+        user = new User({
+            email: email,
+            password: password
+        });
+
+    // Encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+
+    // Return jsonwebtoken
+        res.send('User registered')
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+
+
+
+});
+
+
+module.exports = router;
